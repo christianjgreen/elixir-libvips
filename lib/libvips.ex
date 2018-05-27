0bfc488f298ddf3da62a13094005e197d675dfc3
Arthien <christian.j.green@gmail.com>
2018-05-27 14:55:28 -0400

    expose generic vips_operation function

--- lib/libvips.ex
+++ lib/libvips.ex
@@ -20,20 +20,10 @@ defmodule Libvips do
   def init_vips
   def init_vips, do: exit(:nif_library_not_loaded)
 
-  def nif_smartcrop(file_path, width, height)
-  def nif_smartcrop(_, _, _), do: exit(:nif_library_not_loaded)
+  def nif_smartcrop(file_buf, extension, width, height)
+  def nif_smartcrop(_, _, _, _), do: exit(:nif_library_not_loaded)
 
-  def nif_smartcrop_buffer(image, width, height)
-  def nif_smartcrop_buffer(_, _, _), do: exit(:nif_library_not_loaded)
+  def nif_vips_operation(file_buf, operation, extenion)
+  def nif_vips_operation(_, _, _), do: exit(:nif_library_not_loaded) 
 
-  def smartcrop(file_path, width, height) do
-    file_path
-    |> :binary.bin_to_list()
-    |> nif_smartcrop(width, height)
-  end
-
-  def smartcrop_buffer(file_buf, width, height) when is_binary(file_buf),
-    do: nif_smartcrop_buffer(file_buf, width, height)
-
-  def smartcrop_buffer(_, _, _), do: exit(:nif_library_not_loaded)
 end
--- src/vips.c
+++ src/vips.c
@@ -1,25 +1,66 @@
 #include <erl_nif.h>
 #include <vips/vips.h>
 
-static void smartcrop_path(char path[], int width, int height, char **buf, size_t *len)
-{
-    VipsImage *in = vips_image_new_from_file(path, NULL);
+static void vips_operation(unsigned char **buf, char operation[], char extension[], size_t *len_in, size_t *len_out, unsigned char **buf_out) {
+    VipsImage *in = vips_image_new_from_buffer((void *)*buf, *len_in, "", NULL);
     VipsImage *out;
+    VipsOperation *op;
+    VipsOperation *new_op;
+    GValue gvalue = { 0 };
 
-    vips_smartcrop(in, &out, width, height, NULL);
-    vips_image_write_to_buffer(out, ".png", (void **)buf, len, NULL);
+    op = vips_operation_new(operation);
 
-    g_object_unref(in);
-    g_object_unref(out);
+    /* Init a gvalue as an image, set it to in, use the gvalue to set the
+     * operator property.
+     */
+    g_value_init( &gvalue, VIPS_TYPE_IMAGE );
+    g_value_set_object( &gvalue, in );
+    g_object_set_property( G_OBJECT( op ), "in", &gvalue );
+    g_value_unset( &gvalue );
+
+    /* We no longer need in: op will hold a ref to it as long as it needs
+     * it. 
+     */
+    g_object_unref( in ); 
+
+
+        /* Call the operation. This will look up the operation+args in the vips
+     * operation cache and either return a previous operation, or build
+     * this one. In either case, we have a new ref we mst release.
+     */
+    if( !(new_op = vips_cache_operation_build( op )) ) {
+        g_object_unref( op );
+        vips_error_exit( NULL ); 
+    }
+    g_object_unref( op );
+    op = new_op;
+
+    /* Now get the result from op. g_value_get_object() does not ref the
+     * object, so we need to make a ref for out to hold.
+     */
+    g_value_init( &gvalue, VIPS_TYPE_IMAGE );
+    g_object_get_property( G_OBJECT( op ), "out", &gvalue );
+    out = VIPS_IMAGE( g_value_get_object( &gvalue ) );
+    g_object_ref( out ); 
+    g_value_unset( &gvalue );
+
+    // /* All done: we can unref op. The output objects from op actually hold
+    //  * refs back to it, so before we can unref op, we must unref them. 
+    //  */
+    vips_object_unref_outputs( VIPS_OBJECT( op ) ); 
+    g_object_unref( op );
+
+    vips_image_write_to_buffer(out, extension, (void **)buf_out, len_out, NULL);
+
 }
 
-static void smartcrop_buffer(unsigned char **buf, unsigned char **buf_out, int width, int height, size_t *len_in, size_t *len_out)
+static void smartcrop(unsigned char **buf, unsigned char **buf_out, char extension[], int width, int height, size_t *len_in, size_t *len_out)
 {
     VipsImage *in = vips_image_new_from_buffer((void *)*buf, *len_in, "", NULL);
     VipsImage *out;
 
     vips_smartcrop(in, &out, width, height, NULL);
-    vips_image_write_to_buffer(out, ".png", (void **)buf_out, len_out, NULL);
+    vips_image_write_to_buffer(out, extension, (void **)buf_out, len_out, NULL);
 
     g_object_unref(in);
     g_object_unref(out);
@@ -27,49 +68,51 @@ static void smartcrop_buffer(unsigned char **buf, unsigned char **buf_out, int w
 
 ERL_NIF_TERM nif_smartcrop(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[])
 {
-    char path[2048];
-    char *buf = 0;
-    size_t len = 0;
-
-    unsigned int width, height;
-    ErlNifBinary result_binary;
-    ERL_NIF_TERM result_term;
-
-    if (argc != 3 || !enif_get_string(env, argv[0], path, sizeof(path), ERL_NIF_LATIN1) ||
-        !enif_get_uint(env, argv[1], &width) ||
-        !enif_get_uint(env, argv[2], &height))
-        return enif_make_badarg(env);
-
-    smartcrop_path(path, width, height, &buf, &len);
-
-    enif_alloc_binary(len, &result_binary);
-    memcpy(result_binary.data, buf, len);
-    result_term = enif_make_binary(env, &result_binary);
-    free(buf);
-    return result_term;
-}
-
-ERL_NIF_TERM nif_smartcrop_buffer(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[])
-{
     unsigned char *buf = 0;
+    char extension[1024];
     size_t len_out = 0;
 
     unsigned int width, height;
     ErlNifBinary bin_in, out;
 
-    if (argc != 3 ||
-        !enif_inspect_binary(env, argv[0], &bin_in) ||
-        !enif_get_uint(env, argv[1], &width) ||
-        !enif_get_uint(env, argv[2], &height))
+    if (argc != 4 || !enif_inspect_binary(env, argv[0], &bin_in) ||
+        !enif_get_string(env, argv[1], extension, sizeof(extension), ERL_NIF_LATIN1) ||
+        !enif_get_uint(env, argv[2], &width) ||
+        !enif_get_uint(env, argv[3], &height))
         return enif_make_badarg(env);
 
-    smartcrop_buffer(&bin_in.data, &buf, width, height, &bin_in.size, &len_out);
+
+    smartcrop(&bin_in.data, &buf, extension, width, height, &bin_in.size, &len_out);
     enif_alloc_binary(len_out, &out);
     memcpy(out.data, buf, len_out);
 
     enif_release_binary(&bin_in);
     return enif_make_binary(env, &out);
 }
+
+ERL_NIF_TERM nif_vips_operation(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[])
+{
+    unsigned char *buf = 0;
+    char operation[1024];
+    char extension[1024];
+    size_t len_out = 0;
+
+    ErlNifBinary bin_in, out;
+
+    if (argc != 3 ||
+        !enif_inspect_binary(env, argv[0], &bin_in) ||
+        !enif_get_string(env, argv[1], operation, sizeof(operation), ERL_NIF_LATIN1) ||
+        !enif_get_string(env, argv[2], extension, sizeof(extension), ERL_NIF_LATIN1))
+        return enif_make_badarg(env);
+
+    vips_operation(&bin_in.data, operation, extension, &bin_in.size, &len_out, &buf);
+    enif_alloc_binary(len_out, &out);
+    memcpy(out.data, buf, len_out);
+
+    enif_release_binary(&bin_in);
+    return enif_make_binary(env, &out);
+}
+
 
 ERL_NIF_TERM init_vips(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[])
 {
@@ -78,8 +121,8 @@ ERL_NIF_TERM init_vips(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[])
 }
 
 static ErlNifFunc nif_funcs[] = {
-    {"nif_smartcrop", 3, nif_smartcrop, ERL_NIF_DIRTY_JOB_CPU_BOUND},
-    {"nif_smartcrop_buffer", 3, nif_smartcrop_buffer, ERL_NIF_DIRTY_JOB_CPU_BOUND},
+    {"nif_smartcrop", 4, nif_smartcrop, ERL_NIF_DIRTY_JOB_CPU_BOUND},
+    {"nif_vips_operation", 3, nif_vips_operation, ERL_NIF_DIRTY_JOB_CPU_BOUND},
     {"init_vips", 0, init_vips, ERL_NIF_DIRTY_JOB_IO_BOUND}};
 
 ERL_NIF_INIT(Elixir.Libvips, nif_funcs, NULL, NULL, NULL, NULL);
